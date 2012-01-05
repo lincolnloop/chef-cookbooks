a71cc586206e5a542101d7c49ff31fad7d764b4b
bkonkle <brandon@konkle.us>
2012-01-05 13:11:34 -0600

    Don't set a default IP, because it is merged rather than overridden

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -1,4 +1,4 @@
-default['munin']['server_ips'] = ['127.0.0.1']
+default['munin']['server_ips'] = []  # A list of IP address strings
 
 default['munin']['basedir'] = "/etc/munin"
 default['munin']['root']['group'] = "root"
