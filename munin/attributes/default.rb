d3838625408ab9679f2f9f2124b195b23d1582b5
bkonkle <brandon@konkle.us>
2012-01-08 09:59:13 -0600

    Added override_hostnames to munin

--- munin/attributes/default.rb
+++ munin/attributes/default.rb
@@ -1,4 +1,4 @@
 default['munin']['server_ips'] = []  # A list of IP address strings
-
+default['munin']['override_hostname'] = nil
 default['munin']['basedir'] = "/etc/munin"
 default['munin']['root']['group'] = "root"
